ec01b4b29cf00b2e58a053124590373f8e50a03e
CHChang810716 <CHChang810716@gmail.com>
2021-08-14 11:29:45 +0800

    Initialize AKT-SM

---
+++ cmake/src.cmake
@@ -0,0 +1,8 @@
+set(AKT_SRC_ROOT_DIR "${CMAKE_SOURCE_DIR}/src")
+if(BUILD_TEST)
+    enable_testing()
+endif()
+if(NOT BUILD_TEST)
+    akt_exclude_targets_in(${CMAKE_SOURCE_DIR}/src/test)
+endif()
+akt_src_based_add_targets_in(${CMAKE_SOURCE_DIR}/src ${CMAKE_SOURCE_DIR}/cmake/scheme/exe.cmake)
